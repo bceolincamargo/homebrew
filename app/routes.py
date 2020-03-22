7435bddccca307dc4a5666f9e4e07b9842863982
Bruno Ceolin Camargo <bceolincamargo@gmail.com>
2020-03-22 20:19:06 +1100

    fix Brewing now Search still needing repare

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ from flask_minify import minify
 from htmlmin.minify import html_minify
 from forms import CreateEditBeer, SearchBeer
 from flask import Flask, flash, redirect, render_template, request, url_for
-
+import json
 
 
 class BrewPiLess():
@@ -22,7 +22,8 @@ class BrewPiLess():
         self.externalvolt = str(externalvolt)
         self.tempmode = str(tempmode)
         self.modeinint = str(modeinint) 
-        self.path = '/home/hadoop/repo/brewpiless'
+        #self.path = '/home/hadoop/repo/brewpiless'
+        self.path = 'C:/Users/bruno/Desktop/api'
         
     def run_cmd(self, args_list):
         """
@@ -39,10 +40,13 @@ class BrewPiLess():
         
     def GravaArq(self):
         beername = 'beertest' #replace for input
-        created = time.strftime("%d/%m/%Y, %H:%M:%S")       
-        filename = beername+'.csv'
-        with open(self.path+'/'+filename, 'a+') as arquivo:
-            arquivo.write(beername+';'+created+';'+self.beertemp+';'+self.fridgetemp+';'+self.beerset+';'+self.fridgeset+';'+self.temproom+';'+self.tempaux+';'+self.externalvolt+';'+self.tempmode+';'+self.modeinint+'\n')
+        created = time.strftime("%d/%m/%Y, %H:%M:%S")   
+        arqformat = time.strftime("%d%m%Y%H%M%S")
+        filename = beername+str(arqformat)+'.json'
+        content = {'beername':beername, 'created':created, 'beertemp': self.beertemp, 'fridgetemp': self.fridgetemp, 'beerset':self.beerset,'fridgeset':self.fridgeset, 'temproom':self.temproom, 'tempaux':self.tempaux, 'externalvolt': self.externalvolt, 'tempmode':self.tempmode,'modeinint':self.modeinint, 'finished': ''}
+        with open(self.path+'/'+filename, 'a+') as arquivo:    
+            json.dump(content, arquivo)
+            #arquivo.write(str(beername)+';'+str(created)+';'+str(self.beertemp)+';'+str(self.fridgetemp)+';'+str(self.beerset)+';'+str(self.fridgeset)+';'+str(self.temproom)+';'+str(self.tempaux)+';'+str(self.externalvolt)+';'+str(self.tempmode)+';'+str(self.modeinint)+'\n')
         return filename
                 
     def ToHDFS(self, arquivo):
@@ -92,14 +96,26 @@ def index():
     print("Escala em Inteiro: {}".format(modeinint)) 
 
     dataobj = BrewPiLess(beertemp, fridgetemp, beerset, fridgeset, temproom, tempaux, externalvolt, tempmode, modeinint)
-    #arquivo = dataobj.GravaArq()
+    arquivo = dataobj.GravaArq()
     #dataobj.ToHDFS(arquivo)
     
     return "collecting from brewpiless"
 
 @app.route('/mainpage') # Main Page
 def mainpage():     
-    return render_template('index.html') 
+    conn = pymongo.MongoClient('mongodb://192.168.20.15', 27017)
+    db = conn.brewpiless
+    collection = db.beer
+    res2 = collection.find({"finished": ""}).distinct("beername")
+    res = list(res2) 
+    tudo = collection.find_one({"beername": res[0]})
+    tudodict = dict(tudo)
+    name = tudodict['beername']
+    style = tudodict['beerstyle']
+    desc = tudodict['description']
+    created = tudodict['created']
+
+    return render_template('index.html', name=name, style=style, desc=desc, created=created) 
 
 
 @app.route('/beerrecord', methods=["GET","POST"]) # Beer Cadastro 
@@ -138,8 +154,7 @@ def beersearch():
     form = SearchBeer()     
     beername = form.beername.data
     beerstyle = form.beerstyle.data
-    ret = ''
-    print(form)
+    ret = '' 
     print(str(beername)+" beername")
     print(str(beerstyle)+" beerstyle")
     print(form.validate_on_submit())
@@ -148,20 +163,20 @@ def beersearch():
         if beername == '' and beerstyle == '':
            #busca tudo  
            ret = db.collection.find()
-           flash("anything")
-           print(beerstyle+" style IF")           
+           print(str(ret)+'retorno if')
+           flash("anything")        
         elif beername != '':
            #busca NAME
-           ret = db.collection.find_one(beername)
-           print(ret)
-           print(beerstyle+" style ELSEIF")           
-           flash("elseIF")           
+           print(str(ret)+'retorno elsif')
+           ret = db.collection.find_one({"beername": beername}) 
+           flash("Beername found")           
         else:
         #busca Style
-           ret = db.collection.find_one(beerstyle)     
-           print(ret)
+           retdb = db.collection.find({"beerstyle": beerstyle})     
+           print(str(ret)+'retorno else')
            print(beerstyle+" style else")
-           flash("ELSE")
+           ret = list(retdb)
+           flash("searching by style")
     conn.close()
     return render_template('BrejaSearch.html', form=form, ret=ret) 
                                 
